bae25d6b545a86783e56f40114cf8f41bd2c8395
wangxin <390839834@qq.com>
2020-09-01 22:39:39 +0800

    添加springboot-web模块demo

---
+++ spring-boot/spring-boot-web/src/main/java/com/itxiaobai/wx/config/MyMvcConfig.java
@@ -0,0 +1,6 @@
+package com.itxiaobai.wx.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MyMvcConfig implements WebMvcConfigurer {
+}
---
+++ spring-boot/spring-boot-web/src/test/java/com/itxiaobai/wx/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itxiaobai.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
